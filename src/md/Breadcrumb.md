848b124f7afe9d1b16455fd63fe312d292962c5a
zhensg123 <xyqblog@163.com>
2023-12-13 21:43:52 +0800

    提交

--- src/md/Breadcrumb.md
+++ src/md/Breadcrumb.md
@@ -1,48 +1,42 @@
-## Breadcrumb 面包屑导航
+## Breadcrumb 面包屑导航 展示-导航
+
+Breadcrumb 面包屑导航解释
+
+- 单元是unit-breadcrumb__item，其包含面包屑文字和分隔符
+- 最后隐藏最后一个的分隔符即可
+
 
 ### html 
 
 ```
-<div class="el-breadcrumb">
-    <span class="el-breadcrumb__item">
-      <span :class="['el-breadcrumb__inner']"> 22 </span>
-      <span  class="el-breadcrumb__separator">{{
-        separator
-      }}</span>
-    </span>
-    <span class="el-breadcrumb__item">
-      <span :class="['el-breadcrumb__inner']"> 33 </span>
-      <span  class="el-breadcrumb__separator">{{
-        separator
-      }}</span>
-    </span>
-    <span class="el-breadcrumb__item">
-      <span :class="['el-breadcrumb__inner']"> 44 </span>
-      <span class="el-breadcrumb__separator">{{
-        separator
-      }}</span>
-    </span>
-  </div>
-  <div class="el-breadcrumb">
-    <span class="el-breadcrumb__item">
-      <span :class="['el-breadcrumb__inner']"> 22 </span>
-      <span  class="el-breadcrumb__separator">{{
-        separator1
-      }}</span>
-    </span>
-    <span class="el-breadcrumb__item">
-      <span :class="['el-breadcrumb__inner']"> 33 </span>
-      <span  class="el-breadcrumb__separator">{{
-        separator1
-      }}</span>
-    </span>
-    <span class="el-breadcrumb__item">
-      <span :class="['el-breadcrumb__inner']"> 44 </span>
-      <span class="el-breadcrumb__separator">{{
-        separator1
-      }}</span>
-    </span>
-  </div>
+ <div class="unit-breadcrumb">
+      <span class="unit-breadcrumb__item">
+        <span class="unit-breadcrumb__inner"> 22 </span>
+        <span class="unit-breadcrumb__separator">{{ separator }}</span>
+      </span>
+      <span class="unit-breadcrumb__item">
+        <span class="unit-breadcrumb__inner"> 33 </span>
+        <span class="unit-breadcrumb__separator">{{ separator }}</span>
+      </span>
+      <span class="unit-breadcrumb__item">
+        <span class="unit-breadcrumb__inner"> 44 </span>
+        <span class="unit-breadcrumb__separator">{{ separator }}</span>
+      </span>
+    </div>
+    <div class="unit-breadcrumb">
+      <span class="unit-breadcrumb__item">
+        <span class="unit-breadcrumb__inner"> 22 </span>
+        <span class="unit-breadcrumb__separator">{{ separator1 }}</span>
+      </span>
+      <span class="unit-breadcrumb__item">
+        <span class="unit-breadcrumb__inner"> 33 </span>
+        <span class="unit-breadcrumb__separator">{{ separator1 }}</span>
+      </span>
+      <span class="unit-breadcrumb__item">
+        <span class="unit-breadcrumb__inner"> 44 </span>
+        <span class="unit-breadcrumb__separator">{{ separator1 }}</span>
+      </span>
+    </div>
 ```
 
 ### JavaScript
@@ -63,14 +57,55 @@ export default {
 ### scss
 
 ```
-.el-breadcrumb {
-  display: inline-block;
-  margin: 0;
-  padding: 0;
+.unit-breadcrumb {
   font-size: 14px;
-  line-height: 1.5;
-  list-style: none;
+  line-height: 1;
+}
+.unit-breadcrumb::after,
+.unit-breadcrumb::before {
+  display: table;
+  content: "";
+}
+.unit-breadcrumb::after {
+  clear: both;
+}
+.unit-breadcrumb__separator {
+  margin: 0 9px;
+  font-weight: 700;
+  color: #c0c4cc;
+}
+.unit-breadcrumb__separator[class*="icon"] {
+  margin: 0 6px;
+  font-weight: 400;
+}
+.unit-breadcrumb__item {
+  float: left;
+}
+.unit-breadcrumb__inner {
+  color: #606266;
+}
+.unit-breadcrumb__inner a,
+.unit-breadcrumb__inner.is-link {
+  font-weight: 700;
+  text-decoration: none;
+  -webkit-transition: color 0.2s cubic-bezier(0.645, 0.045, 0.355, 1);
+  transition: color 0.2s cubic-bezier(0.645, 0.045, 0.355, 1);
   color: #303133;
-  white-space: nowrap;
+}
+.unit-breadcrumb__inner a:hover,
+.unit-breadcrumb__inner.is-link:hover {
+  color: #409eff;
+  cursor: pointer;
+}
+.unit-breadcrumb__item:last-child .unit-breadcrumb__inner,
+.unit-breadcrumb__item:last-child .unit-breadcrumb__inner a,
+.unit-breadcrumb__item:last-child .unit-breadcrumb__inner a:hover,
+.unit-breadcrumb__item:last-child .unit-breadcrumb__inner:hover {
+  font-weight: 400;
+  color: #606266;
+  cursor: text;
+}
+.unit-breadcrumb__item:last-child .unit-breadcrumb__separator {
+  display: none;
 }
 ```
--- src/views/show/navigation/Breadcrumb.vue
+++ src/views/show/navigation/Breadcrumb.vue
@@ -2,44 +2,32 @@
   <div v-html="md"></div>
   <h3>效果</h3>
   <div class="review">
-    <div class="el-breadcrumb">
-      <span class="el-breadcrumb__item">
-        <span :class="['el-breadcrumb__inner']"> 22 </span>
-        <span  class="el-breadcrumb__separator">{{
-          separator
-        }}</span>
+    <div class="unit-breadcrumb">
+      <span class="unit-breadcrumb__item">
+        <span class="unit-breadcrumb__inner"> 22 </span>
+        <span class="unit-breadcrumb__separator">{{ separator }}</span>
       </span>
-      <span class="el-breadcrumb__item">
-        <span :class="['el-breadcrumb__inner']"> 33 </span>
-        <span  class="el-breadcrumb__separator">{{
-          separator
-        }}</span>
+      <span class="unit-breadcrumb__item">
+        <span class="unit-breadcrumb__inner"> 33 </span>
+        <span class="unit-breadcrumb__separator">{{ separator }}</span>
       </span>
-      <span class="el-breadcrumb__item">
-        <span :class="['el-breadcrumb__inner']"> 44 </span>
-        <span class="el-breadcrumb__separator">{{
-          separator
-        }}</span>
+      <span class="unit-breadcrumb__item">
+        <span class="unit-breadcrumb__inner"> 44 </span>
+        <span class="unit-breadcrumb__separator">{{ separator }}</span>
       </span>
     </div>
-    <div class="el-breadcrumb">
-      <span class="el-breadcrumb__item">
-        <span :class="['el-breadcrumb__inner']"> 22 </span>
-        <span  class="el-breadcrumb__separator">{{
-          separator1
-        }}</span>
+    <div class="unit-breadcrumb">
+      <span class="unit-breadcrumb__item">
+        <span class="unit-breadcrumb__inner"> 22 </span>
+        <span class="unit-breadcrumb__separator">{{ separator1 }}</span>
       </span>
-      <span class="el-breadcrumb__item">
-        <span :class="['el-breadcrumb__inner']"> 33 </span>
-        <span  class="el-breadcrumb__separator">{{
-          separator1
-        }}</span>
+      <span class="unit-breadcrumb__item">
+        <span class="unit-breadcrumb__inner"> 33 </span>
+        <span class="unit-breadcrumb__separator">{{ separator1 }}</span>
       </span>
-      <span class="el-breadcrumb__item">
-        <span :class="['el-breadcrumb__inner']"> 44 </span>
-        <span class="el-breadcrumb__separator">{{
-          separator1
-        }}</span>
+      <span class="unit-breadcrumb__item">
+        <span class="unit-breadcrumb__inner"> 44 </span>
+        <span class="unit-breadcrumb__separator">{{ separator1 }}</span>
       </span>
     </div>
   </div>
@@ -65,55 +53,55 @@ export default {
 };
 </script>
   <style lang="scss" scoped>
-.el-breadcrumb {
+.unit-breadcrumb {
   font-size: 14px;
   line-height: 1;
 }
-.el-breadcrumb::after,
-.el-breadcrumb::before {
+.unit-breadcrumb::after,
+.unit-breadcrumb::before {
   display: table;
   content: "";
 }
-.el-breadcrumb::after {
+.unit-breadcrumb::after {
   clear: both;
 }
-.el-breadcrumb__separator {
+.unit-breadcrumb__separator {
   margin: 0 9px;
   font-weight: 700;
   color: #c0c4cc;
 }
-.el-breadcrumb__separator[class*="icon"] {
+.unit-breadcrumb__separator[class*="icon"] {
   margin: 0 6px;
   font-weight: 400;
 }
-.el-breadcrumb__item {
+.unit-breadcrumb__item {
   float: left;
 }
-.el-breadcrumb__inner {
+.unit-breadcrumb__inner {
   color: #606266;
 }
-.el-breadcrumb__inner a,
-.el-breadcrumb__inner.is-link {
+.unit-breadcrumb__inner a,
+.unit-breadcrumb__inner.is-link {
   font-weight: 700;
   text-decoration: none;
   -webkit-transition: color 0.2s cubic-bezier(0.645, 0.045, 0.355, 1);
   transition: color 0.2s cubic-bezier(0.645, 0.045, 0.355, 1);
   color: #303133;
 }
-.el-breadcrumb__inner a:hover,
-.el-breadcrumb__inner.is-link:hover {
+.unit-breadcrumb__inner a:hover,
+.unit-breadcrumb__inner.is-link:hover {
   color: #409eff;
   cursor: pointer;
 }
-.el-breadcrumb__item:last-child .el-breadcrumb__inner,
-.el-breadcrumb__item:last-child .el-breadcrumb__inner a,
-.el-breadcrumb__item:last-child .el-breadcrumb__inner a:hover,
-.el-breadcrumb__item:last-child .el-breadcrumb__inner:hover {
+.unit-breadcrumb__item:last-child .unit-breadcrumb__inner,
+.unit-breadcrumb__item:last-child .unit-breadcrumb__inner a,
+.unit-breadcrumb__item:last-child .unit-breadcrumb__inner a:hover,
+.unit-breadcrumb__item:last-child .unit-breadcrumb__inner:hover {
   font-weight: 400;
   color: #606266;
   cursor: text;
 }
-.el-breadcrumb__item:last-child .el-breadcrumb__separator {
+.unit-breadcrumb__item:last-child .unit-breadcrumb__separator {
   display: none;
 }
 </style>
